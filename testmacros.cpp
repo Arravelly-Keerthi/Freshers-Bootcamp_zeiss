d8589455bab1e221a1ffd41ca12b29dcceea1a9e
Arravelly-Keerthi <95274988+Arravelly-Keerthi@users.noreply.github.com>
2024-01-29 13:50:08 +0530

    Create testmacros.cpp

---
+++ testmacros.cpp
@@ -0,0 +1,13 @@
+#include "example.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Enter number :"<<endl;
+  int x; cin>>x;
+  cout<<"Absolute value :"<<ABS(x)<<endl;
+  cout<<"Enter two numbers :"<<endl;
+  int a,b; cin>>a>>b;
+  cout<<"Maximum Value :"<<MAX(a,b)<<endl;
+  return 0;
+}
