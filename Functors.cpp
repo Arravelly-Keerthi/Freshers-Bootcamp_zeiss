0ff6e68598b5e4a80eef1494abc9bee8b67ebfe3
Arravelly-Keerthi <95274988+Arravelly-Keerthi@users.noreply.github.com>
2024-02-02 10:03:41 +0530

    Update Functors.cpp

--- Functors.cpp
+++ Functors.cpp
@@ -8,6 +8,59 @@ using namespace std;
 class IsDivisibleBy {
     int divisor;
 public:
+    IsDivisibleBy() {}
+
+    bool operator()(const string& str) const {
+        return str.length() % divisor == 0;
+    }
+
+    void setDivisor(int divArg) {
+        divisor = divArg;
+    }
+};
+
+vector<string> search(const vector<string>& strings, function<bool(const string&)> criteria) {
+    vector<string> result;
+    for (const string& str : strings) {
+        if (criteria(str)) {
+            result.push_back(str);
+        }
+    }
+    return result;
+}
+
+void printDivisibleStrings(const vector<string>& divisibleStrings) {
+    cout << "Strings with length divisible by divisor: ";
+    for (const string& str : divisibleStrings) {
+        cout << str << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    IsDivisibleBy isDivisibleFunctor;
+    isDivisibleFunctor.setDivisor(3);
+    function<bool(const string&)> isDivisibleByDivisor(isDivisibleFunctor);
+
+    vector<string> strings = {"apple", "banana", "orange", "grape", "kiwi", "pear", "melon"};
+    vector<string> divisibleStrings = search(strings, isDivisibleByDivisor);
+    printDivisibleStrings(divisibleStrings);
+
+    return 0;
+}
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class IsDivisibleBy {
+    int divisor;
+public:
     IsDivisibleBy(){}
 
     bool operator()( string&& str) const {
